8e7fa1878c57aeca37c3faf8324671d2b79ca593
David Church <Davidchurch07@gmail.com>
2017-10-13 19:48:49 +0000

    Starting to work on /pc page & looking at working with json files with tables

--- index.py
+++ index.py
@@ -2,8 +2,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def Home():
+def Index():
   return render_template("index.html"), 200
+
+@app.route("/pc")
+def Pc():
+	return render_template("pc.html"), 200
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
