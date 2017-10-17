e928814986e33d8d78e72bc4f078404d942a29f0
David Church <Davidchurch07@gmail.com>
2017-10-17 18:09:19 +0000

    Added sortable tables for pc, xbox & ps4 pages. Have started to link all the pages together and also started working on the about page and contact page.

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
@@ -8,6 +8,22 @@ def Index():
 @app.route("/pc")
 def Pc():
 	return render_template("pc.html"), 200
+
+@app.route("/xbox")
+def Xbox():
+	return render_template("xbox.html"), 200
+
+@app.route("/ps4")
+def Ps4():
+	return render_template("ps4.html"), 200
+
+@app.route("/about")
+def About():
+	return render_template("ps4.html"), 200
+
+@app.route("/contact")
+def Contact():
+	return render_template("ps4.html"), 200
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
